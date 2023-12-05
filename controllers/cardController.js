b6eaa4e1306c0e94216a1b5904db9cb1659d2856
AsianMustache <phany280687@mail.ru>
2023-12-05 20:40:03 +0300

    fix: test fix 5

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const Card = require("../models/card");
 
 exports.getCards = async (req, res) => {
@@ -25,11 +27,20 @@ exports.createCard = async (req, res) => {
 };
 
 exports.deleteCard = async (req, res) => {
+  const { cardId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(cardId)) {
+    return res.status(400).send({ message: "Некорректный ID" });
+  }
+
   try {
-    const card = await Card.findByIdAndRemove(req.params.cardId);
+    const card = await Card.findById(cardId);
+
     if (!card) {
       return res.status(404).send({ message: "Карточка не найдена" });
     }
+
+    await card.remove();
     res.status(200).send({ message: "Карточка удалена" });
   } catch (err) {
     res.status(500).send({ message: "На сервере произошла ошибка" });
