6e6b721a45a4a83d8419c0a3768017be5f2666d0
AsianMustache <phany280687@mail.ru>
2023-12-17 13:55:06 +0300

    fix: test fix4

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const {
-  createUserSchema,
   updateUserSchema,
   updateAvatarSchema,
 } = require("../validation/validation");
