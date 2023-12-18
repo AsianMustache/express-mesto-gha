7b38852623b1bdd16eb8d5ae8d174d02fc900279
AsianMustache <phany280687@mail.ru>
2023-12-18 15:18:40 +0300

    fix: fix test 23

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -26,20 +26,24 @@
 const validate = (schema) => (req, res, next) => {
   const { error } = schema.validate(req.body);
   if (error) {
-    // Формирование сообщения об ошибке
-    const errorMessage = error.details
-      .map((detail) => {
-        return `${detail.message}`;
-      })
-      .join(", ");
+    // Сбор всех сообщений об ошибках от Joi
+    const errors = error.details.map((err) => {
+      return {
+        field: err.path.join("."),
+        message: err.message.replace(/["']/g, ""),
+      };
+    });
 
-    // Отправка ответа с ошибкой
+    // Отправка кастомного сообщения об ошибке
     res.status(400).json({
-      message: "Ошибка валидации: " + errorMessage,
+      message: "Ошибка валидации поймана при помощи Joi",
+      errors: errors,
     });
   } else {
     next();
   }
 };
+
+module.exports = validate;
 
 module.exports = validate;
