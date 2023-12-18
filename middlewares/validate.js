cd0092ca8f55f8d7db2fd1c7b6bbe671ce3df8bc
AsianMustache <phany280687@mail.ru>
2023-12-18 23:45:28 +0300

    fix: fix test 32

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -26,17 +26,13 @@
 const validate = (schema) => (req, res, next) => {
   const { error } = schema.validate(req.body);
   if (error) {
-    const errors = error.details.map((err) => {
-      // Удаление лишних кавычек из сообщения Joi и форматирование сообщения
-      const message = err.message.replace(/['"]/g, "");
-      return {
-        field: err.path.join("."),
-        message: "Ошибка - name или about должны быть не менее 2 символов",
-      };
-    });
+    const errors = error.details.map((err) => ({
+      field: err.path.join("."),
+      message: err.message,
+    }));
 
     res.status(400).json({
-      message: "Ошибка валидации данных",
+      message: "Ошибка валидации",
       errors: errors,
     });
   } else {
