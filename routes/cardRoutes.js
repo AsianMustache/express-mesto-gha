76b573414a23b561697dc69c25981165e12ef8e6
AsianMustache <phany280687@mail.ru>
2023-12-19 20:29:27 +0300

    fix: fix test 38

--- routes/cardRoutes.js
+++ routes/cardRoutes.js
@@ -1,21 +1,18 @@
 const express = require("express");
-const { celebrate, Joi } = require("celebrate");
+const { celebrate } = require("celebrate");
 const cardController = require("../controllers/cardController");
 const { createCardSchema, cardIdSchema } = require("../validation/validation");
 
 const router = express.Router();
 
 router.get("/", cardController.getCards);
-// router.post("/", cardController.createCard);
 router.post(
   "/",
   celebrate({ body: createCardSchema }),
   cardController.createCard
 );
-router.delete("/:cardId", cardController.deleteCard);
 router.put("/:cardId/likes", celebrate(cardIdSchema), cardController.likeCard);
-// router.put("/:cardId/likes", cardController.likeCard);
-// router.delete("/:cardId/likes", cardController.dislikeCard);
+router.delete("/:cardId", celebrate(cardIdSchema), cardController.deleteCard);
 router.delete(
   "/:cardId/likes",
   celebrate(cardIdSchema),
