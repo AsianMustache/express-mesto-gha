b12130f0a83aab8958bd26c4332900d90000ac71
AsianMustache <phany280687@mail.ru>
2023-12-20 23:34:28 +0300

    fix: review 1(15)

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -54,7 +54,55 @@ const MONGO_DUPLICATE_ERROR_CODE = 11000;
 //   });
 // };
 // eslint-disable-next-line consistent-return
-const errorHandler = (err, req, res, next) => {
+// const errorHandler = (err, req, res, next) => {
+//   // Обработка ошибок валидации Joi
+//   if (err && err.isJoi) {
+//     return res.status(400).json({
+//       message: "Ошибка валидации данных",
+//       errors: err.details.map((detail) => ({
+//         field: detail.path.join("."),
+//         message: detail.message,
+//       })),
+//     });
+//   }
+
+//   // Обработка ошибки дублирования email
+//   if (err.code === MONGO_DUPLICATE_ERROR_CODE) {
+//     return res
+//       .status(http2.constants.HTTP_STATUS_CONFLICT)
+//       .json({ message: "Этот email уже используется" });
+//   }
+
+//   // Обработка неверного токена
+//   if (err.name === "JsonWebTokenError") {
+//     return res
+//       .status(http2.constants.HTTP_STATUS_UNAUTHORIZED)
+//       .jsno({ message: "Некорректный токен" });
+//   }
+
+//   // Обработка кастомных ошибок
+//   if (
+//     err instanceof BadRequestError
+//     || err instanceof NotFoundError
+//     || err instanceof UnauthorizedError
+//     || err instanceof ForbiddenError
+//   ) {
+//     return res.status(err.statusCode).json({ message: err.message });
+//   }
+
+//   // По умолчанию возвращаем 500 ошибку
+//   const statusCode = err.statusCode || http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+//   res.status(statusCode).json({
+//     message:
+//       statusCode === http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR
+//         ? "На сервере произошла ошибка"
+//         : err.message,
+//   });
+//   next();
+// };
+
+// eslint-disable-next-line consistent-return
+const errorHandler = (err, req, res) => {
   // Обработка ошибок валидации Joi
   if (err && err.isJoi) {
     return res.status(400).json({
@@ -77,7 +125,7 @@ const errorHandler = (err, req, res, next) => {
   if (err.name === "JsonWebTokenError") {
     return res
       .status(http2.constants.HTTP_STATUS_UNAUTHORIZED)
-      .jsno({ message: "Некорректный токен" });
+      .json({ message: "Некорректный токен" });
   }
 
   // Обработка кастомных ошибок
@@ -87,24 +135,20 @@ const errorHandler = (err, req, res, next) => {
     || err instanceof UnauthorizedError
     || err instanceof ForbiddenError
   ) {
-    return res.status(err.statusCode).json({ message: err.message });
+    return res.status(err.statusCode || 500).json({ message: err.message });
   }
 
   // По умолчанию возвращаем 500 ошибку
-  const statusCode = err.statusCode || http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
-  res.status(statusCode).json({
-    message:
-      statusCode === http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR
-        ? "На сервере произошла ошибка"
-        : err.message,
-  });
-  next();
+  res
+    .status(500)
+    .json({ message: err.message || "На сервере произошла ошибка" });
 };
 
-module.exports = {
-  errorHandler,
-  BadRequestError,
-  NotFoundError,
-  UnauthorizedError,
-  ForbiddenError,
-};
+// module.exports = {
+//   errorHandler,
+//   BadRequestError,
+//   NotFoundError,
+//   UnauthorizedError,
+//   ForbiddenError,
+// };
+module.exports = errorHandler;
