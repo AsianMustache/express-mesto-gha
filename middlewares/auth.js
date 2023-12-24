e14b6593c042aa2b09cbfa394b702fe11e729312
AsianMustache <phany280687@mail.ru>
2023-12-24 21:06:54 +0300

    fix: review 1(24)

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,33 +1,60 @@
-const jwt = require("jsonwebtoken");
-// const http2 = require("http2");
-const UnauthorizedError = require("../utils/UnauthorizedError");
+// const jwt = require("jsonwebtoken");
+// // const http2 = require("http2");
+// const UnauthorizedError = require("../utils/UnauthorizedError");
+
+// // eslint-disable-next-line consistent-return
+// const auth = (req, res, next) => {
+//   const token = req.headers.authorization
+//     ? req.headers.authorization.replace("Bearer ", "")
+//     : null;
+//   console.log("Полученный токен: ", token);
+//   // if (!token) {
+//   //   console.log("Ошибка: Требуется авторизация");
+//   //   return next(new UnauthorizedError("Требуется авторизация"));
+//   // }
+//   if (!token) {
+//     console.log("Ошибка: Требуется авторизация");
+//     return Promise.reject(new UnauthorizedError("Требуется авторизация")).catch(
+//       next
+//     );
+//   }
+
+//   try {
+//     const payload = jwt.verify(token, "dev_secret");
+//     req.user = payload;
+//     console.log("Токен успешно проверен, payload: ", payload);
+//     next();
+//   } catch (err) {
+//     console.log("Ошибка при проверке токена: ", err);
+//     // next(new UnauthorizedError("Неверный токен"));
+//   }
+// };
+
+// module.exports = auth;
+
+require("dotenv").config();
+import { verify } from "jsonwebtoken";
+import UnauthorizedError from "../utils/UnauthorizedError";
 
 // eslint-disable-next-line consistent-return
-const auth = (req, res, next) => {
-  const token = req.headers.authorization
-    ? req.headers.authorization.replace("Bearer ", "")
-    : null;
-  console.log("Полученный токен: ", token);
-  // if (!token) {
-  //   console.log("Ошибка: Требуется авторизация");
-  //   return next(new UnauthorizedError("Требуется авторизация"));
-  // }
-  if (!token) {
-    console.log("Ошибка: Требуется авторизация");
-    return Promise.reject(new UnauthorizedError("Требуется авторизация")).catch(
-      next
-    );
+export default (req, res, next) => {
+  const { NODE_ENV, JWT_SECRET } = process.env;
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return next(new UnauthorizedError("Необходима авторизация"));
   }
+
+  const token = authorization.replace("Bearer ", "");
 
   try {
-    const payload = jwt.verify(token, "dev_secret");
+    const payload = verify(
+      token,
+      NODE_ENV === "production" ? JWT_SECRET : "dev-secret"
+    );
     req.user = payload;
-    console.log("Токен успешно проверен, payload: ", payload);
     next();
   } catch (err) {
-    console.log("Ошибка при проверке токена: ", err);
-    // next(new UnauthorizedError("Неверный токен"));
+    next(new UnauthorizedError("Необходима авторизация"));
   }
 };
-
-module.exports = auth;
