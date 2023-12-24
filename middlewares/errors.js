d374086eb42eebca78c546385757d7b98b098200
AsianMustache <phany280687@mail.ru>
2023-12-24 21:33:52 +0300

    fix: review 1(26)

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,10 +1,17 @@
-const http2 = require("http2");
+const mongoose = require("mongoose");
+// eslint-disable-next-line import/no-unresolved
+const { StatusCodes } = require("http-status-codes");
 const BadRequestError = require("../utils/BadRequestError");
 const NotFoundError = require("../utils/NotFoundError");
 const UnauthorizedError = require("../utils/UnauthorizedError");
 const ForbiddenError = require("../utils/ForbiddenErrors");
+// const http2 = require("http2");
+// const BadRequestError = require("../utils/BadRequestError");
+// const NotFoundError = require("../utils/NotFoundError");
+// const UnauthorizedError = require("../utils/UnauthorizedError");
+// const ForbiddenError = require("../utils/ForbiddenErrors");
 
-const MONGO_DUPLICATE_ERROR_CODE = 11000;
+// const MONGO_DUPLICATE_ERROR_CODE = 11000;
 
 // eslint-disable-next-line consistent-return
 // module.exports = (err, req, res, next) => {
@@ -101,53 +108,99 @@ const MONGO_DUPLICATE_ERROR_CODE = 11000;
 //   next();
 // };
 
+// // eslint-disable-next-line consistent-return
+// const errorHandler = (err, req, res) => {
+//   // console.log("Ошибка: ", err);
+//   // Обработка ошибок валидации Joi
+//   if (err && err.isJoi) {
+//     console.log("Ошибка валидации Joi: ", err);
+//     return res.status(400).json({
+//       message: "Ошибка валидации данных",
+//       errors: err.details.map((detail) => ({
+//         field: detail.path.join("."),
+//         message: detail.message,
+//       })),
+//     });
+//   }
+
+//   // Обработка ошибки дублирования email
+//   if (err.code === MONGO_DUPLICATE_ERROR_CODE) {
+//     console.log("Ошибка дублирования email: ", err);
+//     return res
+//       .status(http2.constants.HTTP_STATUS_CONFLICT)
+//       .json({ message: "Этот email уже используется" });
+//   }
+
+//   // Обработка неверного токена
+//   if (err.name === "JsonWebTokenError") {
+//     console.log("Ошибка неверного токена: ", err);
+//     return res
+//       .status(http2.constants.HTTP_STATUS_UNAUTHORIZED)
+//       .json({ message: "Некорректный токен" });
+//   }
+
+//   // Обработка кастомных ошибок
+//   if (
+//     err instanceof BadRequestError
+//     || err instanceof NotFoundError
+//     || err instanceof UnauthorizedError
+//     || err instanceof ForbiddenError
+//   ) {
+//     console.log("Кастомная ошибка: ", err);
+//     return res.status(err.status || 500).json({ message: err.message });
+//   }
+
+//   // По умолчанию возвращаем 500 ошибку
+//   console.log("Необработанная ошибка сервера: ", err);
+//   res
+//     .status(500)
+//     .json({ message: err.message || "На сервере произошла ошибка" });
+// };
+
+// module.exports = errorHandler;
+
 // eslint-disable-next-line consistent-return
-const errorHandler = (err, req, res) => {
-  // console.log("Ошибка: ", err);
-  // Обработка ошибок валидации Joi
-  if (err && err.isJoi) {
-    console.log("Ошибка валидации Joi: ", err);
-    return res.status(400).json({
-      message: "Ошибка валидации данных",
-      errors: err.details.map((detail) => ({
-        field: detail.path.join("."),
-        message: detail.message,
-      })),
-    });
+const errorHandler = (err, req, res, next) => {
+  if (err instanceof mongoose.Error) {
+    return res.status(StatusCodes.BAD_REQUEST).send({ message: err.message });
   }
 
-  // Обработка ошибки дублирования email
-  if (err.code === MONGO_DUPLICATE_ERROR_CODE) {
-    console.log("Ошибка дублирования email: ", err);
+  if (err.code === 11000) {
     return res
-      .status(http2.constants.HTTP_STATUS_CONFLICT)
-      .json({ message: "Этот email уже используется" });
+      .status(StatusCodes.CONFLICT)
+      .send({ message: "Этот email уже используется" });
   }
 
-  // Обработка неверного токена
-  if (err.name === "JsonWebTokenError") {
-    console.log("Ошибка неверного токена: ", err);
-    return res
-      .status(http2.constants.HTTP_STATUS_UNAUTHORIZED)
-      .json({ message: "Некорректный токен" });
-  }
-
-  // Обработка кастомных ошибок
   if (
     err instanceof BadRequestError
     || err instanceof NotFoundError
     || err instanceof UnauthorizedError
     || err instanceof ForbiddenError
   ) {
-    console.log("Кастомная ошибка: ", err);
-    return res.status(err.status || 500).json({ message: err.message });
+    return res.status(err.status).send({ message: err.message });
   }
 
-  // По умолчанию возвращаем 500 ошибку
-  console.log("Необработанная ошибка сервера: ", err);
+  if (err.name === "JsonWebTokenError") {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .send({ message: "Некорректный токен" });
+  }
+
+  // Обработка ошибок валидации Joi, если используется
+  if (err.isJoi) {
+    return res.status(StatusCodes.BAD_REQUEST).send({
+      message: "Ошибка валидации данных",
+      details: err.details.map((detail) => ({
+        field: detail.path.join("."),
+        message: detail.message,
+      })),
+    });
+  }
+
+  // Для необработанных ошибок
   res
-    .status(500)
-    .json({ message: err.message || "На сервере произошла ошибка" });
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .send({ message: "На сервере произошла ошибка" });
 };
 
 module.exports = errorHandler;
